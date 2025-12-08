375c4c3b1062878cf0d0286161ff615760a1ddf0
pigbbong <156537276+pigbbong@users.noreply.github.com>
2025-12-08 13:55:26 +0900

    Update current_now.py

--- pipeline/dags/current_now.py
+++ pipeline/dags/current_now.py
@@ -1,28 +1,31 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
+import pendulum
 
-# 실시간(now) 파이프라인 (매 1시간 실행)
+KST = pendulum.timezone("Asia/Seoul")
+
 with DAG(
-    dag_id="current_now",
-    start_date=datetime(2025, 12, 1),
-    schedule="0,30 * * * *",  # 30분마다 실행
+    dag_id="current_after",
+    start_date=datetime(2025, 12, 1, tzinfo=KST),
+    schedule="10 3,9,15,21 * * *",  # KST 기준 03:10, 09:10, 15:10, 21:10
     catchup=False,
+    timezone=KST,
 ):
 
     crawl = BashOperator(
-        task_id="crawl_now",
-        bash_command="python /opt/pipelines/now/crawl_now.py"
+        task_id="crawl_after",
+        bash_command="python /opt/pipelines/after/crawl_after.py"
     )
 
     load_db = BashOperator(
-        task_id="gcs_to_db_now",
-        bash_command="python /opt/pipelines/now/gcs_to_db_now.py"
+        task_id="gcs_to_db_after",
+        bash_command="python /opt/pipelines/after/gcs_to_db_after.py"
     )
 
     refresh_view = BashOperator(
-        task_id="refresh_view_now",
-        bash_command="psql postgresql://airflow:airflow@postgres/weather -f /opt/pipelines/now/view_now.sql"
+        task_id="refresh_view_after",
+        bash_command="psql postgresql://airflow:airflow@postgres/weather -f /opt/pipelines/after/view_after.sql"
     )
 
     crawl >> load_db >> refresh_view
