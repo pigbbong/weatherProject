72e49dced7716ffc5bd854aa4374390644d31cbd
pigbbong <156537276+pigbbong@users.noreply.github.com>
2025-12-08 13:54:38 +0900

    Update monthly_now.py

--- pipeline/dags/monthly_now.py
+++ pipeline/dags/monthly_now.py
@@ -1,12 +1,16 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
+import pendulum
+
+KST = pendulum.timezone("Asia/Seoul")
 
 with DAG(
     dag_id="monthly_now",
-    start_date=datetime(2025, 12, 1),
-    schedule="40 23 L * *",
+    start_date=datetime(2025, 12, 1, tzinfo=KST),
+    schedule="40 23 L * *",  # KST 기준 마지막 날 23:40
     catchup=False,
+    timezone=KST,
 ):
 
     dump_monthly = BashOperator(
