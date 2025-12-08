18d0596d12d23eaadd7121d10957ffda9fabc530
pigbbong <156537276+pigbbong@users.noreply.github.com>
2025-12-08 13:55:17 +0900

    Update daily_now.py

--- pipeline/dags/daily_now.py
+++ pipeline/dags/daily_now.py
@@ -1,12 +1,16 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
+import pendulum
+
+KST = pendulum.timezone("Asia/Seoul")
 
 with DAG(
     dag_id="daily_now",
-    start_date=datetime(2025, 12, 1),
-    schedule="40 23 * * *",  # 매일 23:40
+    start_date=datetime(2025, 12, 1, tzinfo=KST),
+    schedule="40 23 * * *",  # KST 기준 매일 23:40
     catchup=False,
+    timezone=KST,
 ):
 
     dump_daily = BashOperator(
